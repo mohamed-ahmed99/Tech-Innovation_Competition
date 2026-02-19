b41b70ffce2486de31fb377deaa5f87e90ad91d4
Mohamed <masmhdmas@gmail.com>
2026-02-19 21:17:31 +0200

    back-end: verify email

--- back-end/src/controllers/auth.controllers.js
+++ back-end/src/controllers/auth.controllers.js
@@ -41,3 +41,51 @@ export const signUp = asyncHandler(async (req, res) => {
 
 /////////////////// 
 
+// verify email
+export const VerifyEmail = async (req, res) => {
+    // check body
+    console.log(req.body)
+    const {email, code} = req.body
+    if(!email || !code) return res.status(400).json({status:"fail",message:"email and code are required", data:null})
+
+    // check if user in dataBase or varification time expired or not
+    const user = await Users.findOne({"personalInfo.email": email})
+    if(!user) return res.status(404).json({status:"fail", message:"User not found. Please check your email or create a new account."})
+
+    // check code
+    if(code != user.verifyUser.verifyCode) return res.status(401).json({status:"fail", message:"Incorrect verification code", data:null})
+        
+    // token and ip
+    const token = jwt.sign({_id:user._id, email:user.personalInfo.email, role:user.role}, process.env.JWT_SECRET)
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || req.connection.remoteAddress
+     
+
+
+    // create session
+    // const session = {token, ip}
+    // await Sessions.create({user:user._id, sessions:[session]})
+
+    // update DB
+    user.verifyUser = {
+        verifyCode:null,
+        emailVerificationExpires: null,
+    }
+    user.personalInfo.isVerified =  true,
+    await user.save()
+    
+
+
+    // cookies doesn't work on Vercel deployment
+    // res.cookie("MASproAuth", token, {
+    //     httpOnly:true,
+    //     secure:process.env.NODE_ENV === "production",
+    //     sameSite:"None",
+    //     path:"/",
+    //     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    // })
+    
+    // response
+    const userData = {...user.personalInfo, token:token}
+    return res.status(200).json({message: "Verified successfully", user:userData});
+}
+
--- back-end/src/routes/auth.routes.js
+++ back-end/src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import {Router} from 'express'
-import { signUp } from '../controllers/auth.controllers.js'
+import { signUp, VerifyEmail } from '../controllers/auth.controllers.js'
 import { signUpValidator } from '../validators/auth.validators.js'
 
 const authRoutes = Router()
 
 
 authRoutes.post('/signup', signUpValidator, signUp)
+authRoutes.post('/verify-email', VerifyEmail)
 
 
 export default authRoutes
